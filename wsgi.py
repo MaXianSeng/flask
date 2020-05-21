45013036104da253ec656893f100a7b2755caa73
MaXianSeng <197744853@qq.com>
2020-05-21 18:57:17 +0800

    添加反向代理设置

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,6 @@
 from dotenv import load_dotenv
 import os
+from werkzeug.contrib.fixers import ProxyFix #反向代理
 
 dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
 if os.path.exists(dotenv_path):
@@ -8,3 +9,4 @@ if os.path.exists(dotenv_path):
 from bluelog import create_app
 
 app = create_app('production')
+app.wsgi_app = ProxyFix(app.wsgi_app)#方向代理设置
